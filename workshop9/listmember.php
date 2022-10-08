192244ba443a5ac9a20f12e1e94b97937911fe12
Thanawat28 <109870899+Thanawat28@users.noreply.github.com>
2022-10-08 17:37:48 +0700

    Add files via upload

---
+++ workshop9/listmember.php
@@ -0,0 +1,23 @@
+<?php include "connect.php" ?>
+<html>
+
+<body>
+
+    <?php
+    $stmt = $pdo->prepare("SELECT * FROM member");
+    $stmt->execute();
+
+    while ($row = $stmt->fetch()) {
+        echo "ชื่อ" . $row["name"] . "<br>";
+        echo "ชื่อผู้ใช้งาน :" . $row["username"] . "<br>";
+        echo "รหัสผ่าน :" . $row["password"] . "<br>";
+        echo "ที่อยู่ : " . $row["address"] . "<br>";
+        echo "มือถือ :" . $row["mobile"] . "<br>";
+        echo "อีเมล :" . $row["email"] . "<br>";
+        echo "<a href='editform.php?username=" . $row["username"] . "'>แก้ไข</a>" ;
+        echo "<hr>\n";
+    }
+    ?>
+</body>
+
+</html>
